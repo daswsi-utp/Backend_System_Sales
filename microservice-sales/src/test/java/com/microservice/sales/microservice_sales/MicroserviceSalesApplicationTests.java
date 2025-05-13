0734a5fa26cb626c3c3868e7180623eea96fec1e
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-12 22:44:10 -0500

    ADD: Sales Microservice base structure

---
+++ microservice-sales/src/test/java/com/microservice/sales/microservice_sales/MicroserviceSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.sales.microservice_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
