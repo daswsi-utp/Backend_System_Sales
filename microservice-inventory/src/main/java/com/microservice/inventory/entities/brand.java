14eba3bb7ce9aaaa8a168d99f88cfa50fcb07761
Im3Im3 <manuelcode737@gmail.com>
2025-05-15 11:03:46 -0500

    Configurate the class brand

--- microservice-inventory/src/main/java/com/microservice/inventory/entities/brand.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/entities/brand.java
@@ -1,4 +1,24 @@
 package com.microservice.inventory.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "marca")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long  idBrand;
+
+    @Column(name = "nombre", nullable = false)
+    private String nameBrand;
+
+
 }
