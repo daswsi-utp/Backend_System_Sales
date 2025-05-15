bc00002ae1c6a5415ff38caa0db62b61383add52
Im3Im3 <manuelcode737@gmail.com>
2025-05-15 11:14:08 -0500

    Configurate the class category

--- microservice-inventory/src/main/java/com/microservice/inventory/entities/category.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/entities/category.java
@@ -1,4 +1,25 @@
 package com.microservice.inventory.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categorias")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class category {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCategory;
+
+    @Column(name = "nombre", nullable = false)
+    private String nameCategory;
 }
