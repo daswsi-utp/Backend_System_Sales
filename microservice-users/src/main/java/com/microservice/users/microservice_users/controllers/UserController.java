0a0d5ebb30347b5279c40b411e997468987c0957
Richar40K <ricardoismael777@gmail.com>
2025-05-21 18:15:30 -0500

    function to save users

--- microservice-users/src/main/java/com/microservice/users/microservice_users/controllers/UserController.java
+++ microservice-users/src/main/java/com/microservice/users/microservice_users/controllers/UserController.java
@@ -1,7 +1,12 @@
 package com.microservice.users.microservice_users.controllers;
 
+import com.microservice.users.microservice_users.entities.User;
 import com.microservice.users.microservice_users.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ public class UserController
 {
     @Autowired
     private IUserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser =userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
 }
