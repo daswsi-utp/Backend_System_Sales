96e348b9a9c2f1ad766d34ca5a7233145d16692b
Richar40K <ricardoismael777@gmail.com>
2025-05-21 18:01:24 -0500

    dependecy-injection userController

--- microservice-users/src/main/java/com/microservice/users/microservice_users/controllers/UserController.java
+++ microservice-users/src/main/java/com/microservice/users/microservice_users/controllers/UserController.java
@@ -1,7 +1,14 @@
 package com.microservice.users.microservice_users.controllers;
 
+import com.microservice.users.microservice_users.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-public class UserController {
+@RestController
+@RequestMapping("/users")
+public class UserController
+{
+    @Autowired
+    private IUserService userService;
 }
