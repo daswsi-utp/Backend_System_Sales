4fc1e80996114c3f77f6832b0efed0b1d6d1f941
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-21 17:11:05 -0500

    ADDED: Sales Service implementation

--- microservice-sales/src/main/java/com/microservice/sales/microservice_sales/service/SalesServiceImp.java
+++ microservice-sales/src/main/java/com/microservice/sales/microservice_sales/service/SalesServiceImp.java
@@ -1,4 +1,28 @@
 package com.microservice.sales.microservice_sales.service;
 
-public class SalesServiceImp {
+import com.microservice.sales.microservice_sales.entities.Sales;
+import com.microservice.sales.microservice_sales.persistence.SalesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SalesServiceImp implements ISalesService{
+    @Autowired
+    private SalesRepository salesRepository;
+    @Override
+    public List<Sales> findAll() {
+        return (List<Sales>) salesRepository.findAll();
+    }
+
+    @Override
+    public Sales findById(Long id) {
+        return salesRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void save(Sales sale) {
+        salesRepository.save(sale);
+    }
 }
