abea4af2b4e0005c2ea886a3e645f70092ad74b2
Richar40K <ricardoismael777@gmail.com>
2025-05-21 18:20:07 -0500

    function to get all users

--- microservice-users/src/main/java/com/microservice/users/microservice_users/controllers/UserController.java
+++ microservice-users/src/main/java/com/microservice/users/microservice_users/controllers/UserController.java
@@ -5,17 +5,21 @@ import com.microservice.users.microservice_users.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController
 {
     @Autowired
     private IUserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        return ResponseEntity.ok(userService.findAll());
+    }
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user){
