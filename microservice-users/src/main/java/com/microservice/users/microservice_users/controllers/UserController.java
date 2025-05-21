a0474a0b2199e8abcfae7874f73bc8de3926a24b
Richar40K <ricardoismael777@gmail.com>
2025-05-21 18:27:40 -0500

    add function to searh by id user

--- microservice-users/src/main/java/com/microservice/users/microservice_users/controllers/UserController.java
+++ microservice-users/src/main/java/com/microservice/users/microservice_users/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,6 +20,12 @@ public class UserController
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
         return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        Optional<User> user = userService.findById(id);
+        return user.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
