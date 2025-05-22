fc06d31405298ba3f502f2f2f9232c1e2f9bedd8
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-21 19:45:11 -0500

    ADDED: Missing lombok tags

--- microservice-sales/src/main/java/com/microservice/sales/microservice_sales/dto/BrandDTO.java
+++ microservice-sales/src/main/java/com/microservice/sales/microservice_sales/dto/BrandDTO.java
@@ -1,6 +1,13 @@
 package com.microservice.sales.microservice_sales.dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BrandDTO {
     private long  id;
     private String nameBrand;
--- microservice-sales/src/main/java/com/microservice/sales/microservice_sales/dto/CategoryDTO.java
+++ microservice-sales/src/main/java/com/microservice/sales/microservice_sales/dto/CategoryDTO.java
@@ -1,6 +1,14 @@
 package com.microservice.sales.microservice_sales.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryDTO {
     private Long id;
     private String nameCategory;
