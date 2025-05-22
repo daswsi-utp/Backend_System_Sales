a01c243934a5fa87fe0697c31489a8f65fab1a73
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-22 16:37:44 -0500

    FIXED: Feign client error (non-specified package). TODO: Further testing on the API and REST operations

--- microservice-sales/src/main/java/client/ProductClient.java
+++ microservice-sales/src/main/java/client/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "micro-inventory", url = "localhost:8090/api/inventory")
+@FeignClient(name = "micro-inventory", url = "http://localhost:8090/api/inventory")
 public interface ProductClient {
     @GetMapping("/{id}")
     ProductDTO getProductById(@PathVariable Long id);
