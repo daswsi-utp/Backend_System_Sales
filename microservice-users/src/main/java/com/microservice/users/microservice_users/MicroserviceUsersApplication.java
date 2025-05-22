6feb5d26a8da5e19b0759700f1b2c030c29ccc93
Richar40K <ricardoismael777@gmail.com>
2025-05-21 19:27:38 -0500

    encription

--- microservice-users/src/main/java/com/microservice/users/microservice_users/MicroserviceUsersApplication.java
+++ microservice-users/src/main/java/com/microservice/users/microservice_users/MicroserviceUsersApplication.java
@@ -3,11 +3,17 @@ package com.microservice.users.microservice_users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MicroserviceUsersApplication {
-
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceUsersApplication.class, args);
 	}
