2a17e4bae6aaa96d01c44b82916b2f5584a4de46
Richar40K <ricardoismael777@gmail.com>
2025-05-23 17:30:26 -0500

    adding anotations

--- microservice-oauth/src/main/java/com/microservice/oauth/microservice_oauth/security/SecurityConfig.java
+++ microservice-oauth/src/main/java/com/microservice/oauth/microservice_oauth/security/SecurityConfig.java
@@ -1,4 +1,10 @@
 package com.microservice.oauth.microservice_oauth.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+
 public class SecurityConfig {
 }
