2a5103464890471cfb955a70717181ace216e2b2
Richar40K <ricardoismael777@gmail.com>
2025-05-24 18:36:11 -0500

    adding permitd role admin and users

--- microservice-gateway/src/main/java/com/microservice/gateway/microservice_gateway/security/SecurityConfig.java
+++ microservice-gateway/src/main/java/com/microservice/gateway/microservice_gateway/security/SecurityConfig.java
@@ -1,4 +1,28 @@
 package com.microservice.gateway.microservice_gateway.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig
+{
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) throws Exception {
+        return serverHttpSecurity.authorizeExchange(authz -> {
+            authz.pathMatchers("/authorized", "/logout").permitAll().pathMatchers(HttpMethod.GET, "/api/users").permitAll()
+                    .pathMatchers(HttpMethod.GET, "/api/users/{id}").hasAnyRole("ADMIN")
+                    .pathMatchers("/api/users/**").hasAnyRole("ADMIN")
+                    .anyExchange().authenticated();
+
+
+        }).cors(csrf -> csrf.disable())
+                .oauth2Login(withDefaults())
+                .oauth2Client(withDefaults())
+                .oauth2ResourceServer(withDefaults())
+                .build();
+    }
 }
