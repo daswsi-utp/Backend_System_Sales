596843286349af8cbae0358380420452b2457517
Richar40K <ricardoismael777@gmail.com>
2025-05-24 17:53:14 -0500

    adding

--- microservice-gateway/src/main/java/com/microservice/gateway/microservice_gateway/controllers/AppController.java
+++ microservice-gateway/src/main/java/com/microservice/gateway/microservice_gateway/controllers/AppController.java
@@ -4,76 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Controller
 public class AppController
 {
     @GetMapping("/authorized")
-    public Map<String,String> authorizedMap(@RequestParam String codeString){
-        return new Map<String, String>() {
-            @Override
-            public int size() {
-                return 0;
-            }
+    public Map<String,String> authorizedMap(@RequestParam String code){
+          Map<String, String> map =new HashMap<>();
 
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
+          map.put("code",code);
+          return map;
 
-            @Override
-            public boolean containsKey(Object key) {
-                return false;
-            }
 
-            @Override
-            public boolean containsValue(Object value) {
-                return false;
-            }
-
-            @Override
-            public String get(Object key) {
-                return "";
-            }
-
-            @Override
-            public String put(String key, String value) {
-                return "";
-            }
-
-            @Override
-            public String remove(Object key) {
-                return "";
-            }
-
-            @Override
-            public void putAll(Map<? extends String, ? extends String> m) {
-
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public Set<String> keySet() {
-                return Set.of();
-            }
-
-            @Override
-            public Collection<String> values() {
-                return List.of();
-            }
-
-            @Override
-            public Set<Entry<String, String>> entrySet() {
-                return Set.of();
-            }
-        }
     }
 }
