643de39718a03ce19c0011388425e88f5738b358
jackutp <u21318614@utp.edu.pe>
2025-05-24 18:15:23 -0500

    add service

---
+++ microservice-orders/src/main/java/com/microservice/orders/service/IOrderService.java
@@ -0,0 +1,11 @@
+package com.microservice.orders.service;
+
+import com.microservice.orders.entities.Order;
+
+import java.util.List;
+
+public interface IOrderService {
+    List<Order> findAll();
+    Order findById(Long id);
+    void save (Order order);
+}
