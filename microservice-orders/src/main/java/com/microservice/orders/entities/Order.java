70f33a5af8f33f1c3b9a1e0011e135cf4080253a
jackutp <u21318614@utp.edu.pe>
2025-05-24 18:09:37 -0500

    add class order

--- microservice-orders/src/main/java/com/microservice/orders/entities/Order.java
+++ microservice-orders/src/main/java/com/microservice/orders/entities/Order.java
@@ -1,5 +1,25 @@
 package com.microservice.orders.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Builder
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nameStaff;
+    @Column(name = "order_date")
+    private Timestamp date;
+    private Double total;
+    private String status;
 
 }
