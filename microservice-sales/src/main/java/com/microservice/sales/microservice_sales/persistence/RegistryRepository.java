57b721c4c64e827fd39caf33d5fd36ce2b410e84
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-25 18:59:56 -0500

    REMOVED: Redundant Repositories for: User and Registry

--- microservice-sales/src/main/java/com/microservice/sales/microservice_sales/persistence/RegistryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.sales.microservice_sales.persistence;
-
-import com.microservice.sales.microservice_sales.entities.Registry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistryRepository extends JpaRepository<Registry, Long> {
-}
--- microservice-sales/src/main/java/com/microservice/sales/microservice_sales/persistence/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.sales.microservice_sales.persistence;
-
-import com.microservice.sales.microservice_sales.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
