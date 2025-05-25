4f86d9f7780a967f904ed1977085ed025c640210
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-25 14:16:12 -0500

    ADDED: User entity class

--- microservice-registry/src/main/java/com/microservice/registry/microservice_registry/entitites/User.java
+++ microservice-registry/src/main/java/com/microservice/registry/microservice_registry/entitites/User.java
@@ -1,4 +1,31 @@
 package com.microservice.registry.microservice_registry.entitites;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Date;
+
+@Entity
+@Data
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long idUser;
+    private String name;
+    @Column(name = "middle_name")
+    private String middleName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "birth_date")
+    private Date birthDate;
+    private String dni;
+    private String email;
+    private String password;
 }
