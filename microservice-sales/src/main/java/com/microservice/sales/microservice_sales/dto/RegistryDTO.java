1c0d7e698c39fb6b7cbbd966f77a6f3a67479091
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-25 13:07:52 -0500

    ADDED: Temporary registry DTO. Dedicated microservice will be added soon

---
+++ microservice-sales/src/main/java/com/microservice/sales/microservice_sales/dto/RegistryDTO.java
@@ -0,0 +1,17 @@
+package com.microservice.sales.microservice_sales.dto;
+
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegistryDTO {
+    private String type;
+    private Timestamp registrationDate;
+    private Long userId;
+    private String templateUrl;
+}
