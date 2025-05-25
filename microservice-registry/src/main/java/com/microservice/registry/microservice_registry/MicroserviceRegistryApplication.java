1a90907a151e7d4f020b0bbab0789cb124cfe413
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-25 13:32:41 -0500

    ADDED: Base structure of registry microservice

---
+++ microservice-registry/src/main/java/com/microservice/registry/microservice_registry/MicroserviceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.registry.microservice_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRegistryApplication.class, args);
+	}
+
+}
