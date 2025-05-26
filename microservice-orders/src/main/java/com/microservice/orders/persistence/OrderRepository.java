e87f2071250e45f69b887d221e6e148ced013a95
jackutp <u21318614@utp.edu.pe>
2025-05-26 15:55:57 -0500

    3

--- microservice-orders/src/main/java/com/microservice/orders/persistence/OrderRepository.java
+++ microservice-orders/src/main/java/com/microservice/orders/persistence/OrderRepository.java
@@ -1,14 +1,11 @@
 package com.microservice.orders.persistence;
 
 import com.microservice.orders.entities.Order;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
-    List<Order>findAllByProductId(Long productId);
 }
