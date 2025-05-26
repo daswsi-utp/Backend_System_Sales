9fff4ce07b34fb51ccf55ac0039063e41a52d113
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-25 22:26:43 -0500

    ADDED: Final touches for endpoint to decrease stock. TODO: Further testing

--- microservice-inventory/src/main/java/com/microservice/inventory/service/ServiceProductImpl.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/service/ServiceProductImpl.java
@@ -1,5 +1,6 @@
 package com.microservice.inventory.service;
 
+import com.microservice.inventory.dto.ProductStockUpdateDTO;
 import com.microservice.inventory.entities.Product;
 import com.microservice.inventory.persistence.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,5 +64,19 @@ public class ServiceProductImpl implements IServiceProduct {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void decreaseStock(List<ProductStockUpdateDTO> stockUpdates) {
+        for(ProductStockUpdateDTO update : stockUpdates){
+            Product product = productRepository.findById(update.getProductId()).orElseThrow(() -> new RuntimeException("Product not found with this ID: " + update.getProductId()));
+            int newQuantity = product.getQuantityProduct() - update.getQuantity();
+            if(newQuantity < 0){
+                throw new IllegalStateException("Insufficient stock");
+            }
+
+            product.setQuantityProduct(newQuantity);
+            productRepository.save(product);
+        }
     }
 }
