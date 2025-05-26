5b48881be7375562f7caff6b7d0bbd20e0fd2a5f
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-25 19:07:50 -0500

    MODIFIED: Registry entity class reduced to debloat now that a dedicated microservice exists

--- microservice-sales/src/main/java/com/microservice/sales/microservice_sales/entities/Registry.java
+++ microservice-sales/src/main/java/com/microservice/sales/microservice_sales/entities/Registry.java
@@ -16,15 +16,6 @@ import java.sql.Timestamp;
 @Table(name = "registry")
 public class Registry {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "registry_id")
     private Long idRegistry;
-    private String type;
-    @Column(name = "registration_date")
-    private Timestamp registrationDate;
-    @OneToOne
-    @JoinColumn(name = "fk_user", referencedColumnName = "user_id", nullable = false)
-    private User user;
-    @Column(name = "template_url")
-    private String templateUrl;
 }
