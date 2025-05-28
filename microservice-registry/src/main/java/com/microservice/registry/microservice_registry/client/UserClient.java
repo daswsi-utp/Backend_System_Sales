8b8e4c93a78eee98cc0049de7b8405fbc6ed6544
KenyiPlatanazo <riositalo2@gmail.com>
2025-05-28 18:19:38 -0500

    ADDED: Added files to retrieve and maintain user data to be displayed later. Also, a specific port was added to the user microservice for testing

---
+++ microservice-registry/src/main/java/com/microservice/registry/microservice_registry/client/UserClient.java
@@ -0,0 +1,12 @@
+package com.microservice.registry.microservice_registry.client;
+
+import com.microservice.registry.microservice_registry.dto.UserDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "microservice-users", url = "http://localhost:8092/api/products")
+public interface UserClient {
+    @GetMapping("/{id}")
+    UserDTO getUserById(@PathVariable Long id);
+}
