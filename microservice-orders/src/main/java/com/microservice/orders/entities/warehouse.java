6e4573d01a1a3e524e36155f17e1a12b3d51b035
jackutp <u21318614@utp.edu.pe>
2025-05-29 16:46:04 -0500

    add attributes warehouse

--- microservice-orders/src/main/java/com/microservice/orders/entities/warehouse.java
+++ microservice-orders/src/main/java/com/microservice/orders/entities/warehouse.java
@@ -1,4 +1,26 @@
 package com.microservice.orders.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class warehouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "warehouse_id")
+    private Long idProvider;
+    @Column(name = "name")
+    private String nameWarehouse;
+    @Column(name = "address")
+    private String address;
+
 }
