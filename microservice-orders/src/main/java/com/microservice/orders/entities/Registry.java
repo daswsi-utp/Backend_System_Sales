e4fdde6720454c3cc2dd8ffa2160a86aec69c4d2
jackutp <u21318614@utp.edu.pe>
2025-05-29 16:51:06 -0500

    add attributes registry

--- microservice-orders/src/main/java/com/microservice/orders/entities/Registry.java
+++ microservice-orders/src/main/java/com/microservice/orders/entities/Registry.java
@@ -1,6 +1,8 @@
 package com.microservice.orders.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,4 +16,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Table(name = "registry")
 public class Registry {
+    @Id
+    @Column(name = "registry_id")
+    private Long idRegistry;
 }
